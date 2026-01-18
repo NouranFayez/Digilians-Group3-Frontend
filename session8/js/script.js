f7ef68b761e9b61c8c0d9f29cf7099c3e672b3db
NouranFayez <nouran_ahmed269@hotmail.com>
2026-01-18 13:49:51 +0200

    session8

---
+++ session8/js/script.js
@@ -0,0 +1,59 @@
+console.log('from external')
+
+
+// var - let - const 
+
+
+var firstName = "Nouran" 
+
+// console.log(firstName)
+
+// dataType ==> string , number , boolean (true / false) , undefined , null
+var userAge = 31
+
+var isGraduated = true
+
+var h 
+
+// console.log(h)
+
+var m = null
+
+// var p = prompt("enter your name" , "userName")
+// var c = ""
+// console.log(p)
+
+// Mathematical Operators (+ - / * ** % ++ --)
+var n = 20
+var m = 30
+var sum = n + m
+var k = "dfghjkjhgf"
+// console.log(m / k) // NaN ==> Not a Number
+
+// console.log(2**2)
+
+// increment ++  decrement --
+
+var b = 20
+console.log(b++) // 20
+console.log(b) // 21
+console.log(++b) // 22
+
+// Comparison Operators (> >= < <= == === != !==)
+
+var k = 20
+var m = 20
+var n = "20"
+
+// console.log(m > k)
+// console.log(m >= k)
+
+console.log(m == n) // value
+console.log(m === n) // value - data type
+
+console.log(m != n) //  value 
+console.log(m !== n) // value - data type 
+
+
+
+
