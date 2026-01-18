06a0ef3f3a7e77c546be140b7e0405cb18859edd
NouranFayez <nouran_ahmed269@hotmail.com>
2026-01-18 14:39:57 +0200

    session8

--- session8/js/script.js
+++ session8/js/script.js
@@ -4,7 +4,7 @@ console.log('from external')
 // var - let - const 
 
 
-var firstName = "Nouran" 
+var firstName = "Nouran"
 
 // console.log(firstName)
 
@@ -13,7 +13,7 @@ var userAge = 31
 
 var isGraduated = true
 
-var h 
+var h
 
 // console.log(h)
 
@@ -35,9 +35,9 @@ var k = "dfghjkjhgf"
 // increment ++  decrement --
 
 var b = 20
-console.log(b++) // 20
-console.log(b) // 21
-console.log(++b) // 22
+// console.log(b++) // 20
+// console.log(b) // 21
+// console.log(++b) // 22
 
 // Comparison Operators (> >= < <= == === != !==)
 
@@ -48,12 +48,97 @@ var n = "20"
 // console.log(m > k)
 // console.log(m >= k)
 
-console.log(m == n) // value
-console.log(m === n) // value - data type
+// console.log(m == n) // value
+// console.log(m === n) // value - data type
 
-console.log(m != n) //  value 
-console.log(m !== n) // value - data type 
+// console.log(m != n) //  value 
+// console.log(m !== n) // value - data type 
+
+//  logical Operators (&& || !)
+var fName = "ali"
+var lastName = "ahmed"
+
+// console.log(fName == 'ali') // true
+// console.log(lastName == 'Mohamed') // false
+
+// console.log(fName == 'ali' &&  lastName == 'Mohamed' )
+// console.log(fName == 'ali' ||  lastName == 'Mohamed' )
+
+// console.log(!(fName == "Zein"))
+
+// if condition
+
+// if(condition/s){
+//     true
+// }
+// else {
+//     false
+// }
+
+// var p = prompt('enter your name')
+// if(p == 'ahmed'){
+//     var res = p
+//      console.log(res)
+
+// }
+//     else{
+//         console.log('invalid user')
+//     } 
 
 
+
+// var job = prompt('enter your Job')
+// eng engineer 
+// doc doctor
+// dev developer
+
+// if(job == 'eng' || job == "engineer"){
+//     console.log('engineer')
+// }
+// else if(job == 'doc' || job == "doctor"){
+//     console.log('doctor')
+// }
+// else if(job == 'dev' || job == "developer"){
+//     console.log('developer')
+// }
+// else console.log('invalid job')
+
+
+
+/////////// task 
+// var degree = prompt('enter your degree')
+// null
+// empty String
+// 90 - 100 ==> A
+// 80 - 90 ==> B 
+// 70 - 80 ==> c 
+// < 70 ==> F
+// > 100 ==> invalid degree
+
+
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+// console.log(7)
+// console.log(8)
+
+
+
+// for(variable ; condition ; increment/decrement){
+//     code ...
+// }
+
+// 1 - 10
+for(var i = 1 ; i <= 10 ; i++ ){
+    // console.log('javascript')
+    if(i == 8){
+        break
+    } 
+    else if(i == 3) continue
+    console.log(i)
+}
 
 
