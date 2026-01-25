e03bd59945f5562577c080a773c6d4e76df82fcb
NouranFayez <nouran_ahmed269@hotmail.com>
2026-01-25 12:44:37 +0200

    session10

---
+++ session10/js/script.js
@@ -0,0 +1,48 @@
+var ids = [3 , 5, 10 , 6]
+// for(var i = 0 ; i< ids.length ; i++){
+//     console.log(ids[i])
+// }
+// ids.forEach((item , i , arr)=>{
+//     // console.log(item)
+//     // console.log(i)
+//     // console.log(arr)
+//     if(item > 6) console.log(item > 6)
+// })
+
+// ()=>{
+
+// }
+// var ids = [3 , 5, 10 , 20 ,  6]
+
+// var element = ids.find((ele , ind , arr)=>{
+//     return ele > 6
+// })
+
+// var element = ids.find((ele)=> ele > 6)
+// var element = ids.find((ele)=> ele > 60) // undefined
+
+
+// var index = ids.findIndex((ele)=> ele > 6)
+// // var index = ids.findIndex((ele)=> ele > 60) // -1
+
+// var newArray = ids.filter(item => item > 6)
+// var newArray = ids.filter(item => item > 60) // []
+
+// console.log(newArray)
+// map ==> search
+
+// var user = 
+// ["Ahmed" , "Mohamed" , "Fayez" , "nasr city" , "October" , 70]
+
+var userObj = {
+    firstName : 'Ahmed' , 
+    lastName : 'Mohamed' ,
+    homeAddress : "Nasr city",
+    age : 70
+}
+
+console.log(userObj)
+console.log(userObj.age)
+
+
+
