98b9e11b7d981a963097556a2c4adc1fa5ea65ef
NouranFayez <nouran_ahmed269@hotmail.com>
2026-01-26 12:30:02 +0200

    session11

---
+++ session11/js/script.js
@@ -0,0 +1,40 @@
+// var fName = "Ahmed"
+// localStorage.setItem("userName" , 'Zein')
+// localStorage.setItem("userAddress" , 'Maadi')
+// localStorage.setItem("userAge" , '10')
+
+// console.log(localStorage.getItem("userAge"));
+// console.log(localStorage.getItem("list"));
+
+var users = [
+    {name : "Ahmed" , id : 1 , balance : 1000},
+    {name : "marwa" , id : 2 , balance : 1000},
+    {name : "zein" , id : 3 , balance : 1000},
+]
+
+
+// var test = ["html" , 2 , true ]
+
+// console.log(test.toString());
+
+// console.log(JSON.stringify(users));
+
+// localStorage.setItem('usersList' , JSON.stringify(users))
+
+// console.log(localStorage.getItem('usersList').push({name :'ay 7aga' , id: 4 , balance : 1000}));
+// console.log(JSON.parse(localStorage.getItem('usersList')));
+
+
+
+
+// localStorage.removeItem('userName')
+
+// sessionStorage.setItem('UserName' , "Zein")
+
+
+
+
+
+
+
+
