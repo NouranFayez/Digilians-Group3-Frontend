5c7d259cf43a50699f7bd54dcaaa24e6711d92f7
NouranFayez <nouran_ahmed269@hotmail.com>
2026-03-09 12:43:39 +0200

    session21

--- session21-React/src/About.jsx
+++ session21-React/src/About.jsx
@@ -1,23 +1,33 @@
 
-
+import { useState } from "react"
+import "./About.css"
 export const About = ({ userName, age, addressList }) => {
     // console.log(props)
     // let userName = props.userName
     // let age = props.age
     console.log(addressList)
+    // let flag = true
+   let [flag , setFlag] = useState(true)
+    function changeColor(){
+        setFlag(!flag)
+    }
 
     // let {userName , age} = props 
     return (
         <>
-            <h2>Welcome {userName}</h2>
-            <h2>age {age}</h2>
+            <h2 style={{backgroundColor:'lightgray'}}>Welcome {userName}</h2>
+            <h2 className={flag ? "text-blue" : "text-red"}>age {age}</h2>
+            <button onClick={changeColor}>Change text color</button>
             <ul>
                 {
                     // addressList.map((item)=>{
                     //   return   <li>{item}</li>
                     // })
                     addressList.map((item , i) => 
-                        (<li key={i}>{item}</li>) 
+                        (<li className="text-blue" key={i}>{item}
+                            
+                        </li>) 
+
                     )
 
                 }
